f3e2724be9471d60e73c02e1a006db19756a6c0f
thunderstorm.shen <thunderstorm.shen@derbysoft.net>
2023-02-27 13:06:37 +0800

    init

---
+++ src/test/java/com/example/ninuxgithubfacility/NinuxGithubFacilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ninuxgithubfacility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinuxGithubFacilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
